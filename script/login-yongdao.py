d58d68633c0e6c548362d8275adb144f12ee24f1
Gaoxiang <512560771@qq.com>
2021-12-02 10:29:57 +0800

    从数据库获取学员信息

--- script/login-yongdao.py
+++ script/login-yongdao.py
@@ -8,11 +8,11 @@ import base64
 import random
 import json
 
-base_url = "https://we.chinaedu.net/volbeacon"
+yongdao_url = "https://we.chinaedu.net/volbeacon"
+api_url = "http://39.105.22.135:8000"
 task_dict = {}
 # 任务列表，保存每天的登录任务
 job_list = []
-
 
 def _str2key(s):
     # 对字符串解码
@@ -63,7 +63,7 @@ def login(login_id, password_encryption, user_name):
         "loginId": (None, login_id),
         "passwordEncryption": (None, password_encryption)
     }
-    res = requests.post(base_url + url, files=data)
+    res = requests.post(yongdao_url + url, files=data)
     if res.status_code == 200:
         print(user_name + " 登录成功！")
 
@@ -71,7 +71,7 @@ def login(login_id, password_encryption, user_name):
 # 获取公钥
 def login_encrypt(task_time):
     url = "/common/loginEncrypt.do"
-    res = requests.post(base_url + url)
+    res = requests.post(yongdao_url + url)
     public_rsa_key = res.json()['publicRSAKey']
     login_id = res.json()['loginId']
     user = task_dict[task_time]
@@ -88,8 +88,8 @@ ip = socket.gethostbyname(hostname)
 
 # 随机时间
 def round_time():
-    h = random.randint(9, 12)
-    m = random.randint(0, 59)
+    h = random.randint(10, 12)
+    m = random.randint(30, 59)
     task_time = str(h).zfill(2) + ":" + str(m).zfill(2)
     return task_time
 
@@ -108,23 +108,23 @@ def getCurrentDay():
 def init_job():
     cancel_job()
     task_dict.clear()
-    user_list = json.load(open("./user-config.json"))
-    # print(user_list)
+    # user_list = json.load(open("./user-config.json"))
+    user_list = json.loads(requests.get(api_url + "/student/list").text)
     # 任务字典 key未执行任务的时间点，value未登录用户信息{user_name: String, password: String}
     for user in user_list:
         task_time = round_time()
         # 去重
         while task_time in task_dict.keys():
             task_time = round_time()
-        print(getCurrentDay() + ' ' + task_time + " 登录用户" + user["user_name"])
+        print(getCurrentDay() + ' ' + task_time + " 登录用户" + user["studentName"])
         task_dict[task_time] = user
         job = schedule.every().day.at(task_time).do(login_encrypt, task_time)
         job_list.append(job)
     # print(task_dict, job_list)
 
 
-# init_job()
-schedule.every().day.at("00:01").do(init_job)
+init_job()
+# schedule.every().day.at("00:01").do(init_job)
 while True:
     schedule.run_pending()
     time.sleep(1)
